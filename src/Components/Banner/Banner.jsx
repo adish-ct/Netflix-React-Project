3e9569f15202317000b3bbf3bae35fae6efaca20
adish-ct <adishathu525@gmail.com>
2023-10-14 20:01:55 +0530

    added image base url

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,29 +4,27 @@ import axios from '../../Axios'
 import { API_KEY } from '../Constants/Constants'
 
 function Banner() {
-
-    const [movie, setMovie] = useState()
+    const [movie, setMovie] = useState();
 
     useEffect(() => {
-        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((responce) => {
-            setMovie(responce.data.results[0])
-        })
-    }, [])
-
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
+            setMovie(response.data.results[1]);
+        });
+    }, []);
 
     return (
-        <div className='banner'>
+        <div className="banner">
             <div className="content">
-                <h1 className="title"> {movie.title} </h1>
+                <h1 className="title">{movie ? movie.title : 'Loading...'}</h1>
                 <div className="banner-buttons">
                     <button className="button">Play</button>
                     <button className="button">My List</button>
                 </div>
-                <h1 className="description"> {movie} </h1>
+                <h1 className="description">{movie ? movie.overview : 'Loading...'}</h1>
             </div>
             <div className="fade-bottom"></div>
         </div>
-    )
+    );
 }
 
-export default Banner
+export default Banner;
--- src/Components/Constants/Constants.jsx
+++ src/Components/Constants/Constants.jsx
@@ -1,2 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3/';
-export const API_KEY = 'ad105e5fc80c61ec60747b279e0b1fd3'
+export const API_KEY = 'ad105e5fc80c61ec60747b279e0b1fd3';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
