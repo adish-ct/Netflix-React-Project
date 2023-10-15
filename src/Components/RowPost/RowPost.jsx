25adc1d160b4f6c37e4d8ccf3fa59e8f2a19423d
adish-ct <adishathu525@gmail.com>
2023-10-15 23:08:10 +0530

    create notes.txt add componets

--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -8,6 +8,8 @@ import YouTube, { YouTubeProps } from 'react-youtube'
 function RowPost(props) {
 
     const [movie, setMovie] = useState([])
+    // create state for storing perticular youtube ID
+    const [key, setKey] = useState('')
 
     useEffect(() => {
         // getting original movie with api calling, with our API key
@@ -28,12 +30,23 @@ function RowPost(props) {
 
 
     const opts = {
-        height: '390',
-        width: '640',
+        height: '500',
+        width: '100%',
         playerVars: {
             autoplay: 1,
         },
     };
+
+    const movieTrailer = (id) => {
+        console.log(id);
+        axios.get(`movie/${id}/videos?api_key=${API_KEY}`).then((response) => {
+            if (response.data.results.length !== 0) {
+                setKey(response.data.results[0].key)
+            } else {
+                console.log("array is empty");
+            }
+        })
+    }
 
     return (
         <div className='row-post'>
@@ -45,12 +58,12 @@ function RowPost(props) {
                             {/* we have a base url for images and specific url for each single movie so we concatanate with base url and 
                             single move url, backdrop_path is the image url for current movie that stored in movie object
                              */}
-                            <img className={props.isSmall ? 'is-small' : 'poster'} src={`${imageUrl + movie.backdrop_path}`} alt="posters image" />
+                            <img onClick={() => movieTrailer(movie.id)} className={props.isSmall ? 'is-small' : 'poster'} src={`${imageUrl + movie.backdrop_path}`} alt="posters image" />
                         </div>
                     )
                 })}
             </div>
-            <YouTube videoId="2g811Eo7K8U" opts={opts} />
+            {key && <YouTube videoId={key} opts={opts} />}
         </div>
     )
 }
