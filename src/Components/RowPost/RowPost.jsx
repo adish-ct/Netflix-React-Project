f07129a0530f77582fb1841a10a726fd7dbdda7d
adish-ct <adishathu525@gmail.com>
2023-10-15 13:29:24 +0530

    create state for storing movies

--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -1,17 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './RowPost.css'
 import axios, { Axios } from '../../Axios'
 import { API_KEY, baseUrl } from '../Constants/Constants'
 
 function RowPost() {
 
+    const [movie, setMovie] = useState([])
+
     useEffect(() => {
         // getting original movie with api calling, with our API key
+        // we used axios instance which one we created in our Axios.jsx file, axios helps us to make HTTP request
         axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(resoponse => {
-            console.log(resoponse);
+            console.log(resoponse.data.results);
+            // storing the movies into the state, results contains movies in array format
+            setMovie(resoponse.data.results)
         }).catch(err => {
-            alert(err)
+            // showing error
+            // alert(err)
         })
+        // catch field helps us to find errors that faced while we are trying to make the HTTP responce.
 
     }, [])
 
